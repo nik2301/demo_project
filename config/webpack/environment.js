91654355bffba3ba30d90076f20b7d3b756d805a
nikhil <nikhilpariyani2301@gmail.com>
2021-11-09 14:02:33 +0530

    bootstrap & jqery added through yarn

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+import {ProvidePlugin} from 'webpack'
+environment.plugins.append('Provide',
+    new ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        Popper: ['popper.js', 'default']
+    })
+    )
+
+
+
 module.exports = environment
