c63194f6ffca9743bcbc6843e1c9d48304ff12a9
nikhil <nikhilpariyani2301@gmail.com>
2021-11-10 14:03:00 +0530

    roles added to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  enum role: [:member, :trainer]
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= :member 
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
